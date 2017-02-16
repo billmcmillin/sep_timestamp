498d00e041cd4e28b16a83ceeca6a509e4cc867e
Bill McMillin <billmcmillin@gmail.com>
2017-02-16 16:47:34 -0500

    add 14-16 set

--- sep.py
+++ sep.py
@@ -3,7 +3,7 @@ import time
 import datetime
 
 with open('Crime_Incidents_Condensed.csv', mode='r') as infile:
-	with open('Crime_Incidents_Cleaned_2016.csv', mode='w') as outfile:
+	with open('Crime_Incidents_Cleaned_14-16.csv', mode='w') as outfile:
 		writer = csv.writer(outfile)
 		reader = csv.reader(infile)
                 headers = 0
@@ -12,18 +12,22 @@ with open('Crime_Incidents_Condensed.csv', mode='r') as infile:
                     if headers == 0:
                         headers = 1
                         writer.writerow(row)
-                    elif row[4]: 
-                        tstamp = datetime.datetime.strptime(row[4],"%m/%d/%y %H:%M")
+                    #make sure there is a date committed
+                    elif row[4]:
+                        #tstamp = datetime.datetime.strptime(row[4],"%m/%d/%y %H:%M")
+                        tstamp = datetime.datetime.strptime(row[4],"%Y-%m-%d %H:%M")
                         block = row[5] + '_' + row[6] + '_' + row[7]
-                        print block
+                        doy = tstamp.timetuple().tm_yday
                         row[14] = tstamp.year
                         row[15] = tstamp.month
                         row[16] = tstamp.day
                         row[17] = tstamp.weekday()
-                        row[18] = tstamp.time()
-                        row[19] = tstamp.hour
-                        row[20] = block
-                        if row[14] == 2016:
+                        row[18] = doy
+                        row[19] = tstamp.time()
+                        row[20] = tstamp.hour
+                        row[21] = block
+                        if row[14] == 2016 or row[14] == 2015 or row[14] == 2014:
+                            print row
                             writer.writerow(row)
         outfile.close()
 infile.close()
